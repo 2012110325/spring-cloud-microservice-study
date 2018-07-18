14d07b3a598964b020198a4239467bd8fd6f5b88
liqiang <liqiang@kkworld.com>
2018-07-18 18:20:06 +0800

    first spring cloud

---
+++ src/test/java/com/example/springcloud/microservicestudy/SpringCloudMicroserviceStudyApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.springcloud.microservicestudy;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringCloudMicroserviceStudyApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
